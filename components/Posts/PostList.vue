eb38b7ee061819934bdbfbaf8060f128187cc635
wtildestar <wtildestar@gmail.com>
2018-12-29 20:46:10 +0500

    initial commit

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+      id="1"
+      :is-admin="isAdmin"
+      thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfKqgVZkNSm4SwUGuR94lDPfijbyY6SO2ezJeddc37sSilFOTu"
+      title="hello there!"
+      previewText="this is my first post!"
+    />
+    <PostPreview
+      id="2"
+      :is-admin="isAdmin"
+      thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfKqgVZkNSm4SwUGuR94lDPfijbyY6SO2ezJeddc37sSilFOTu"
+      title="hello there! 2"
+      previewText="this is my second post!"
+    />
+    <PostPreview
+      id="3"
+      :is-admin="isAdmin"
+      thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfKqgVZkNSm4SwUGuR94lDPfijbyY6SO2ezJeddc37sSilFOTu"
+      title="hello there! 3"
+      previewText="this is my third post!"
+    />
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+    components: {
+      PostPreview
+    },
+    props: {
+      isAdmin: {
+        type: Boolean,
+        default: false
+      }
+    }
+}
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost"/>
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from "@/components/Admin/AdminPostForm";
+export default {
+  layout: 'admin',
+  components: {
+    AdminPostForm
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: "wtildestar",
+        title: "Мое приложение",
+        content: "Очень круто, спасибо! ",
+        thumbnailLink:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfKqgVZkNSm4SwUGuR94lDPfijbyY6SO2ezJeddc37sSilFOTu"
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
